d19a9f24aca76a26207281b6e18d11fbf95dabc1
yasharthgupta10@gmail.com <yasharthgupta10@gmail.com>
2024-08-24 14:30:59 +0530

    sbus.c: lib for sbus related operations

---
+++ lib/sbus.c
@@ -0,0 +1,74 @@
+#include <stdint.h>
+
+// creating sbus packets from msg queue
+int sbus_parsing() {
+
+	uint8_t packet[25],	// to store sbus packet
+		start = 0x0F,  // start byte 
+		end = 0x00,   // end byte
+		message=0,   // to store a byte from queue
+		i;
+
+	k_msgq_get(&uart_msgq, &message,K_MSEC(4));
+
+	if(message == start) {
+		for(i = 0; i < 25; i++) {
+			packet[i] = message;
+			k_msgq_get(&uart_msgq, &message,K_MSEC(4));
+		}
+		// check if the last byte was 0x00
+		if(packet[25] == end)
+		ch = parse_buffer(packet);
+		return 1;
+	}
+
+	else 
+		return 0;
+}
+
+// creating 11 bit channel
+uint16_t *parse_buffer(uint8_t buff[]) { 
+		
+	// to store channels
+	uint16_t channel[16];
+
+	// masking byte shiftings bits (value in hexa '0x07FF')
+	uint16_t mask = 0x7ff;
+
+	// creating channels 
+        channel[0]  = ((buff[1] | buff[2]<<8)                 & mask);
+        channel[1]  = ((buff[2]>>3 | buff[3]<<5)              & mask);
+        channel[2]  = ((buff[3]>>6 | buff[4]<<2 | buff[5]<<10) & mask);
+        channel[3]  = ((buff[5]>>1 | buff[6]<<7)              & mask);
+        channel[4]  = ((buff[6]>>4 | buff[7]<<4)              & mask);
+        channel[5]  = ((buff[7]>>7 | buff[8]<<1 | buff[9]<<9)  & mask);
+        channel[6]  = ((buff[9]>>2 | buff[10]<<6)             & mask);
+        channel[7]  = ((buff[10]>>5| buff[11]<<3)             & mask);
+        channel[8]  = ((buff[12]   | buff[13]<<8)             & mask);
+        channel[9]  = ((buff[13]>>3| buff[14]<<5)             & mask);
+        channel[10] = ((buff[14]>>6| buff[15]<<2| buff[16]<<10)& mask);
+        channel[11] = ((buff[16]>>1| buff[17]<<7)             & mask);
+        channel[12] = ((buff[17]>>4| buff[18]<<4)             & mask);
+        channel[13] = ((buff[18]>>7| buff[19]<<1| buff[20]<<9) & mask);
+        channel[14] = ((buff[20]>>2| buff[21]<<6)             & mask);
+        channel[15] = ((buff[21]>>5| buff[22]<<3)             & mask);
+
+	return channel;
+}
+/*
+// to check parity byte
+int parity_checker(int parity)
+{
+	uint8_t failsafe, frame_lost, bit0, bit1, ct;
+
+	// extracting first two bit of parity byte
+	bit0 = parity>>7;
+	bit1 = (parity>>6)<<1;
+
+	if (bit0 == 0 && bit1 == 0)
+		return 0;
+
+	else
+		return 1;
+}
+*/
