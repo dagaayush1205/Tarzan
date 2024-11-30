996ca284b2f1158779f33cd9482fdcd87c42d04f
yasharthgupta10@gmail.com <yasharthgupta10@gmail.com>
2024-11-30 20:36:45 +0530

    drive.c : removed Stepper_motor_write

--- include/Tarzan/lib/drive.h
+++ include/Tarzan/lib/drive.h
@@ -5,6 +5,3 @@ float sbus_velocity_interpolation(uint16_t channel, float *velocity_range,
 
 uint32_t sbus_pwm_interpolation(uint16_t channel, uint32_t *pwm_range,
                                 uint16_t *channel_range);
-
-int Stepper_motor_write(const struct stepper_motor *motor, uint16_t ch,
-                        int pos);
