f898833527a965abb420de47eded34b8328a7dba
Yasharth011 <yasharthgupta10@gmail.com>
2025-11-17 04:51:05 +0530

    pragma once

--- include/Tarzan/lib/drive.h
+++ include/Tarzan/lib/drive.h
@@ -1,22 +1,24 @@
-#include <stdint.h>
-#include <kyvernitis/lib/kyvernitis.h>
+#pragma once
 #include <Tarzan/lib/jerk_limiter.h>
 #include <Tarzan/lib/scurve_planner.h>
+#include <kyvernitis/lib/kyvernitis.h>
+#include <stdint.h>
 
 struct DiffDriveCtrl {
-    jerk_limiter_t linear_limiter;
-    jerk_limiter_t angular_limiter;
-    scurve_profile_t linear_profile;
-    scurve_profile_t angular_profile;
-    bool is_auto_active;
-    float move_timer;
+  jerk_limiter_t linear_limiter;
+  jerk_limiter_t angular_limiter;
+  scurve_profile_t linear_profile;
+  scurve_profile_t angular_profile;
+  bool is_auto_active;
+  float move_timer;
 };
 
 struct DiffDriveCtx {
-   struct DiffDriveConfig drive_config; 
-   struct DiffDriveCtrl drive_control;
-   int64_t previous_update_timestamp;
-   int (*velocity_callback)(const float *velocity_buffer, int buffer_len, int wheels_per_side);
+  struct DiffDriveConfig drive_config;
+  struct DiffDriveCtrl drive_control;
+  int64_t previous_update_timestamp;
+  int (*velocity_callback)(const float *velocity_buffer, int buffer_len,
+                           int wheels_per_side);
 };
 
 enum msg_type { AUTONOMOUS, INVERSE, IMU };
@@ -27,8 +29,10 @@ float sbus_velocity_interpolation(uint16_t channel, float *velocity_range,
 uint32_t sbus_pwm_interpolation(uint16_t channel, uint32_t *pwm_range,
                                 uint16_t *channel_range);
 
-struct DiffDriveCtx* drive_init(struct DiffDriveConfig *config, int (*velocity_callback)(const float *velocity_buffer, int buffer_len, int wheels_per_side));
+struct DiffDriveCtx *
+drive_init(struct DiffDriveConfig *config,
+           int (*velocity_callback)(const float *velocity_buffer,
+                                    int buffer_len, int wheels_per_side));
 
-int diffdrive_kine(struct DiffDriveCtx* ctx, struct DiffDriveTwist command, float dt_sec);
-
-
+int diffdrive_kine(struct DiffDriveCtx *ctx, struct DiffDriveTwist command,
+                   float dt_sec);
